dc715018b540da9511a324ba4e823ca9eb35dfbe
HaiTran <nguyenhai200902@gmail.com>
2023-12-29 09:57:26 +0700

    yooo

--- src/main/java/com/example/PBL6/util/HttpUtils.java
+++ src/main/java/com/example/PBL6/util/HttpUtils.java
@@ -2,5 +2,5 @@ package com.example.PBL6.util;
 
 public class HttpUtils {
     public static final String BASE_URL = "https://clothesshop.up.railway.app";
-    public static final String WEB_URL = "https://clothesshop.netlify.app";
+    public static final String WEB_URL = "https://clothesshopdut.netlify.app";
 }
